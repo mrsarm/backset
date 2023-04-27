d311838a612089eb673b99b3f077d032116ac0d9
Mariano Ruiz <mrsarm@gmail.com>
2023-04-27 16:05:18 -0300

    Avoid re-serialize static response

--- src/health.rs
+++ src/health.rs
@@ -3,13 +3,20 @@ use serde_json::json;
 
 use crate::BACKSET_VERSION;
 
-#[get("")]
-async fn health_check_handler() -> impl Responder {
-    HttpResponse::Ok().json(json!({
+lazy_static! {
+    static ref HEALTH_CHECK: String = json!({
         "status": "UP",
         "service": "backset",
-        "version": BACKSET_VERSION
-    }))
+        "version": BACKSET_VERSION,
+    })
+    .to_string();
+}
+
+#[get("")]
+async fn health_check_handler() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(HEALTH_CHECK.as_str())
 }
 
 pub fn config(conf: &mut web::ServiceConfig) {
