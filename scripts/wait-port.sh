757d9da9366bab1f00fb938a26f9d68d70c1e3ea
Mariano Ruiz <mrsarm@gmail.com>
2023-08-30 13:37:59 -0300

    Wait a bit less, up to 30 secs

--- scripts/wait-port.sh
+++ scripts/wait-port.sh
@@ -3,5 +3,5 @@
 # wait-port.sh PORT
 #
 
-sleep 2
-echo -n "Waiting port $1 ... "; for _ in `seq 1 100`; do echo -n .; sleep 0.5; nc -z localhost $1 && echo " Open." && exit ; done; echo " Timeout!" >&2; exit 1
+sleep 1
+echo -n "Waiting port $1 ... "; for _ in `seq 1 60`; do echo -n .; sleep 0.5; nc -z localhost $1 && echo " Open." && exit ; done; echo " Timeout!" >&2; exit 1
