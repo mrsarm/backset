31d0269cda72583f0ca1cf1234af26401abd7ae0
Mariano Ruiz <mrsarm@gmail.com>
2023-08-30 13:00:40 -0300

    Wait a bit more

--- scripts/wait-port.sh
+++ scripts/wait-port.sh
@@ -3,4 +3,5 @@
 # wait-port.sh PORT
 #
 
-echo -n "Waiting port $1 ... "; for _ in `seq 1 100`; do echo -n .; sleep 0.25; nc -z localhost $1 && echo " Open." && exit ; done; echo " Timeout!" >&2; exit 1
+sleep 2
+echo -n "Waiting port $1 ... "; for _ in `seq 1 100`; do echo -n .; sleep 0.5; nc -z localhost $1 && echo " Open." && exit ; done; echo " Timeout!" >&2; exit 1
