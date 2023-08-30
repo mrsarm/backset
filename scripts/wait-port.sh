dbbd4efe2747a05869ce365a2a2b67dcd709b914
Mariano Ruiz <mrsarm@gmail.com>
2023-08-30 11:52:38 -0300

    Fix Postgres wait

---
+++ scripts/wait-port.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# wait-port.sh PORT
+#
+
+echo -n "Waiting port $1 ... "; for _ in `seq 1 100`; do echo -n .; sleep 0.25; nc -z localhost $1 && echo " Open." && exit ; done; echo " Timeout!" >&2; exit 1
