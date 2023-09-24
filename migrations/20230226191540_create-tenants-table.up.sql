eac16a070e55d46c545f55d07d2d54a584caa57c
Mariano Ruiz <mrsarm@gmail.com>
2023-09-24 18:42:30 -0300

    Fix tenants migrations

--- migrations/20230226191540_create-tenants-table.up.sql
+++ migrations/20230226191540_create-tenants-table.up.sql
@@ -1,6 +1,8 @@
-CREATE TABLE tenants (
+CREATE TABLE IF NOT EXISTS tenants (
     -- id BIGSERIAL PRIMARY KEY,  --> `pub id: i64` in the Rust model
-    id VARCHAR(40) NOT NULL UNIQUE,
+    id VARCHAR(40) PRIMARY KEY,
     name VARCHAR(80) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS tenants_created_at_idx ON tenants (created_at DESC);
